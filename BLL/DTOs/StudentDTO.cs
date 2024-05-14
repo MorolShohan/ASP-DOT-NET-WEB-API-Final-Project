4d9af9214a06e3b60cab831d9c4fd416a4529761
Thanvir007 <thanvirahmed.aiubcse@gmail.com>
2024-05-14 14:34:16 +0600

    adding student file

---
+++ BLL/DTOs/StudentDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class StudentDTO
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(50, ErrorMessage = "Email cannot exceed 50 characters")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid date format")]
+        public DateTime DateOfBirth { get; set; }
+
+        [StringLength(15, ErrorMessage = "Phone number cannot exceed 15 characters")]
+        public string Phone { get; set; }
+
+        [StringLength(100, ErrorMessage = "Institution name cannot exceed 100 characters")]
+        public string InstitutionName { get; set; }
+
+        [StringLength(255, ErrorMessage = "Address cannot exceed 255 characters")]
+        public string Address { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 50 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Invalid gender")]
+        public string Gender { get; set; }
+
+
+    }
+
+
+}
+
